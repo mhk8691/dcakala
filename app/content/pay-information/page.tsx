d6236ed966123fd2ea533fee2343b5db85d653ba
mhk8691 <mohammadhosseinkaboli86@gmail.com>
2024-10-22 21:18:10 +0330

    adding pay-information page

--- app/content/pay-information/page.tsx
+++ app/content/pay-information/page.tsx
@@ -1,5 +1,18 @@
 import React from "react";
-
+import { metadata } from "../../layout";
+import { Box, Typography } from "@mui/material";
+metadata.title = "تنوع در روش های پرداخت";
 export default function PayInformationPage() {
-  return <div>page</div>;
+  return (
+    <Box sx={{ mt: 5, direction: "rtl", px: 2 }}>
+      <Typography variant="body1" color="initial">
+        نماد های اعتباری سایت دی سی ای کالا
+      </Typography>
+      <Typography variant="body1" color="initial">
+        وب سایت دی سی ای کالا با دریافت مجوزهای معتبر از کلیه نهاد های نظارتی در
+        راستای جلب اعتمادو اعتبار از مشتریان و آسودگی خیال آنها آماده خدمت رسانی
+        به مشتریان عزیز میباشد
+      </Typography>
+    </Box>
+  );
 }
