6adfe564a2baa06d0a9c0469b1797b5bf0f37ca9
mhk8691 <mohammadhosseinkaboli86@gmail.com>
2024-10-22 21:19:46 +0330

    adding refund-pay page

--- app/content/refund-pay/page.tsx
+++ app/content/refund-pay/page.tsx
@@ -1,5 +1,13 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
-
+import { metadata } from "../../layout";
+metadata.title =' تضمین بازگشت وجه';
 export default function RefundPayPage() {
-  return <div>page</div>;
+  return (
+    <Box sx={{ mt: 5, direction: "rtl", px: 5 }}>
+      <Typography variant="body1" color="initial">
+        بازگشت 7 روزه در صو.رت مغایرت کالا
+      </Typography>
+    </Box>
+  );
 }
