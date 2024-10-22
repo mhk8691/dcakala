34c8c8145dc14f35a20f578ba6918e68129c7f4c
mhk8691 <mohammadhosseinkaboli86@gmail.com>
2024-10-22 21:21:15 +0330

    adding shipping-ways-info page

--- app/content/shipping-ways-info/page.tsx
+++ app/content/shipping-ways-info/page.tsx
@@ -1,5 +1,51 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
-
+import { metadata } from "../../layout";
+metadata.title = "ارسال سریع به سراسر ایران";
 export default function ShippingWaysInfoPage() {
-  return <div>page</div>;
+  return (
+    <Box
+      sx={{
+        px: 2,
+        mt: 5,
+      }}
+    >
+      <Typography
+        variant="body1"
+        color="initial"
+        sx={{
+          direction: "rtl",
+        }}
+      >
+        - پیک اختصاصی (برای مناطق 22 گانه شهر تهران)
+      </Typography>
+      <Typography
+        variant="body1"
+        color="initial"
+        sx={{
+          direction: "rtl",
+        }}
+      >
+        - پست پیشتاز (برای سراسر کشور)
+      </Typography>
+      <Typography
+        variant="body1"
+        color="initial"
+        sx={{
+          direction: "rtl",
+        }}
+      >
+        - تیپاکس و چاپار (برای شهرهای تحت پوشش تیپاکس)
+      </Typography>
+      <Typography
+        variant="body1"
+        color="initial"
+        sx={{
+          direction: "rtl",
+        }}
+      >
+        - باربری (برای شهرهای تحت پوشش باربری مناسب برای اجناس سنگین)
+      </Typography>
+    </Box>
+  );
 }
